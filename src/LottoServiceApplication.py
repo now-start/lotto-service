a889d3bfdc6ff6d2bffe291acdecacc86dd42734
moon <nowstart2691@gmail.com>
2024-06-13 00:08:08 +0900

    fix(v3.2.1): package structure

--- src/LottoServiceApplication.py
+++ src/LottoServiceApplication.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from module.Buy import LottoBuyer
 from module.Result import LottoResultChecker
 
