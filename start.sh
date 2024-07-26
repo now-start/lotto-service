d33fb2c61ba7efc04fbb29d45919e33816b2eae2
moon <nowstart2691@lguplus.co.kr>
2024-07-26 11:01:07 +0900

    fix(v3.2.11): start.sh 변경

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 python /app/src/module/Email.py
-echo "$LOTTO_SCHEDULE python /app/src/LottoServiceApplication.py >> /var/log/cron.log 2>&1" | crontab -
+(
+  env
+  echo "$LOTTO_SCHEDULE python /app/src/LottoServiceApplication.py >> /var/log/cron.log 2>&1"
+) | crontab -
 cron -f
