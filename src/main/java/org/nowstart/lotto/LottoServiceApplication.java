2c38410df525d4555d80b5543ac8d5c70ed32913
moon <nowstart2691@gmail.com>
2025-01-25 16:54:21 +0900

    feat: 스케줄러 동작

--- src/main/java/org/nowstart/lotto/LottoServiceApplication.java
+++ src/main/java/org/nowstart/lotto/LottoServiceApplication.java
@@ -10,8 +10,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
+@EnableScheduling
 @EnableDiscoveryClient
 @SpringBootApplication
 @RequiredArgsConstructor
