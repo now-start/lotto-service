76fd3d0c865ccf54f9c8026c4e17482048de659e
moon <nowstart2691@gmail.com>
2025-09-06 19:44:27 +0900

    feat: 버그수정

--- src/main/java/org/nowstart/lotto/service/GoogleNotifyService.java
+++ src/main/java/org/nowstart/lotto/service/GoogleNotifyService.java
@@ -28,16 +28,27 @@ public class GoogleNotifyService {
      * @throws UnsupportedEncodingException UnsupportedEncodingException
      */
     public void send(MessageDto message) throws MessagingException, UnsupportedEncodingException {
-        MimeMessageHelper helper = new MimeMessageHelper(javaMailSender.createMimeMessage(), true, "UTF-8");
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(javaMailSender.createMimeMessage(), true, "UTF-8");
 
-        helper.setTo(lottoProperties.getEmail());
-        helper.setSubject(message.getSubject());
-        helper.setText(message.getText());
-        if (message.getLottoImage() != null) {
-            helper.addInline(message.getContentId(), message.getLottoImage());
-            helper.setText(message.getImageText(), true);
+            helper.setTo(lottoProperties.getEmail());
+            helper.setSubject(message.getSubject());
+            helper.setText(message.getText());
+            if (message.getLottoImage() != null) {
+                helper.addInline(message.getContentId(), message.getLottoImage());
+                helper.setText(message.getImageText(), true);
+            }
+
+            javaMailSender.send(helper.getMimeMessage());
+            log.info("메일 발송 완료 - 제목: {}", message.getSubject());
+        } catch (Exception e) {
+            if (e.getMessage().contains("Daily user sending limit exceeded") ||
+                    e.getMessage().contains("550")) {
+                log.warn("Gmail 일일 발송 제한 초과 - 메일 발송 스킵: {}", message.getSubject());
+            } else {
+                log.error("메일 발송 실패 - 제목: {}, 오류: {}", message.getSubject(), e.getMessage());
+                throw e;
+            }
         }
-
-        javaMailSender.send(helper.getMimeMessage());
     }
 }
